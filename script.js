c849e0245ea9d86112d3dc93d4cdaf0e8f9dc3a6
D Singh <leveragegroupinc@gmail.com>
2023-08-08 13:06:00 -0700

    Add function to play 5 rounds

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ function playRound(playerSelection, computerSelection) {
         if (pSelection == 'scissors'){
             switch (computerSelection){
                 case 'rock':
+                    computerScore++
                     return 'rock beats scissors.You lose!'
                 case 'paper':
+                    playerScore++
                     return 'scissors beat paper. You win!'
                 case 'scissors':
                     return 'both scissors. You tie!'    
@@ -28,25 +30,51 @@ function playRound(playerSelection, computerSelection) {
         else if(pSelection == 'paper'){
             switch (computerSelection){
                 case 'rock':
+                    playerScore++
                     return 'paper beats rock.You win!'
                 case 'paper':
                     return 'both paper. You tie!'
                 case 'scissors':
+                    computerScore++
                     return 'scissors beat paper. You lose!'
-        }else if(pSelection == 'rock'){
+            }
+        }
+        else if(pSelection == 'rock'){
             switch (computerSelection){
                 case 'rock':
                     return 'both rock.You tie!'
                 case 'paper':
+                    computerScore++
                     return 'paper beats rock. You lose!'
                 case 'scissors':
+                    playerScore++
                     return 'rock beats scissors. You win!'
-        }else{
+            }
+        }
+        else{
             return 'Invalid selection. Please chose between rock , paper, scissors'
         }
 
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+  function game(){
+
+    let playerSelection, computerSelection;
+
+    for(let i=0; i<5;i++){
+        playerSelection = prompt("chose between rock,paper or scissors")
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("Computer score: "+computerScore,"Player score: "+playerScore)
+    }
+
+    if(computerScore>playerScore)
+        console.log("Computer wins!")
+    else if (playerScore>computerScore)
+        console.log("You Win!")
+    else
+        console.log("it's a tie")
+  }
+  
+  let computerScore = 0, playerScore = 0;
+  game();
